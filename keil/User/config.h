c220edfb261c5a13da80668ca7515eeca316f75a
maksyuki <myc20404401@sina.com>
2018-03-17 09:55:42 +0800

    Modify user config file

--- keil/User/config.h
+++ keil/User/config.h
@@ -1,3 +1,21 @@
-#define SYSTEM_INTERRUPT_VERSION
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
 
+#ifndef SYSTEM_INTERRUPT_VERSION
+#define SYSTEM_INTERRUPT_VERSION
+#endif
+
+#ifndef M_PI
 #define M_PI 3.1415926
+#endif
+
+#ifndef MPU6050_DMP
+#define MPU6050_DMP
+#endif
+
+#ifndef YAW_CORRECT
+#define YAW_CORRECT
+#endif
+
+#endif
+
