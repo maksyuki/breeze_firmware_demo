a29506a27e37561d7abb75005645057cf46f4f83
maksyuki <myc20404401@sina.com>
2018-04-02 15:45:41 +0800

    Modify STM32 user's config

--- keil/User/config.h
+++ keil/User/config.h
@@ -6,11 +6,15 @@
 #endif
 
 #ifndef M_PI
-#define M_PI 3.1415926
+#define M_PI 3.1415926F
 #endif
 
-#ifndef MPU6050_DMP
-#define MPU6050_DMP
+//#ifndef MPU6050_DMP
+//#define MPU6050_DMP
+//#endif
+
+#ifndef NRF51822_BLUETOOTH_MODE
+#define NRF51822_BLUETOOTH_MODE
 #endif
 
 #ifndef YAW_CORRECT
