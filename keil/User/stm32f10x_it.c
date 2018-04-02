7750e582c85763020d19d5c3bc80a0710fb45850
maksyuki <myc20404401@sina.com>
2018-04-02 15:44:13 +0800

    Add TIM1 func and serial func

--- keil/User/stm32f10x_it.c
+++ keil/User/stm32f10x_it.c
@@ -16,9 +16,11 @@ History:
 myyerrol    2017.04.24    Modify the module
 *******************************************************************************/
 
+#include <stdio.h>
 #include "config.h"
 #include "stm32f10x_it.h"
-// #include "stm32f10x_driver_timer.h"
+#include "stm32f10x_driver_timer.h"
+#include "stm32f10x_driver_delay.h"
 #include "stm32f10x_driver_usart.h"
 #include "stm32f10x_module_led.h"
 
@@ -35,6 +37,35 @@ void SysTick_Handler(void)
 #ifdef SYSTEM_INTERRUPT_VERSION
     it_systick_uptime++;
 #endif
+}
+
+void TIM1_UP_IRQHandler(void)
+{
+    if (TIM_GetITStatus(TIM1, TIM_IT_Update) == SET)
+    {
+        timer_dmp++;
+        if (++timer_loop_cnt_100hz * 200 >= 1000)
+        {
+            timer_loop_cnt_100hz  = 0;
+            timer_loop_flag_100hz = true;
+        }
+        if (++timer_loop_cnt_50hz * 100 >= 1000)
+        {
+            timer_loop_cnt_50hz  = 0;
+            timer_loop_flag_50hz = true;
+        }
+        if (++timer_loop_cnt_20hz * 40 >= 1000)
+        {
+            timer_loop_cnt_20hz  = 0;
+            timer_loop_flag_20hz = true;
+        }
+        if (++timer_loop_cnt_10hz * 20 >= 1000)
+        {
+            timer_loop_cnt_10hz  = 0;
+            timer_loop_flag_10hz = true;
+        }
+        TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
+    }
 }
 
 void TIM3_IRQHandler(void)
@@ -81,8 +112,36 @@ void USART1_IRQHandler(void)
     }
     else if (USART_GetITStatus(USART1, USART_IT_RXNE) == SET)
     {
-        USART_WriteBuffer(&USART_RingBufferRxStructure,
-                         (u8)USART_ReceiveData(USART1));
+        
+        //if(tmpch == 'a') LED_A_TOGGLE;
+        //if(tmpch == 'b') LED_B_TOGGLE;
+        //debug USART_SendData(USART1, (u8)USART_ReceiveData(USART1));
+        
+        
+        if (!usart_rx_completion_flag) // need to continue to receive data, if the `usart_rx_completion_flag is true,
+        {                   // it means the data haven't to be dealt with yet.
+            u8 tmpch = (u8) USART_ReceiveData(USART1);
+            if (tmpch == '$')
+            {
+                USART_WriteBuffer(&USART_RingBufferRxStructure, tmpch);
+                usart_rx_start_at_first = 1;
+            }
+            else
+            {
+                if (usart_rx_start_at_first)
+                {
+                    USART_WriteBuffer(&USART_RingBufferRxStructure, tmpch);
+                    if (tmpch == '<') 
+                    {
+                        usart_rx_completion_flag = 1;
+                        usart_rx_start_at_first  = 0;
+                    }
+                }
+            }
+        }
+        
         USART_ClearITPendingBit(USART1, USART_IT_RXNE);
+        
+        // debug printf("\r\nI have received data!\r\n");
     }
 }
