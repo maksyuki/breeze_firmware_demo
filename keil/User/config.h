cd004cce5874066458bd90c91f2fbb1b4cae1ff1
maksyuki <myc20404401@sina.com>
2018-05-19 17:00:19 +0800

    Format codes

--- keil/User/config.h
+++ keil/User/config.h
@@ -5,6 +5,18 @@
 #define SYSTEM_INTERRUPT_VERSION
 #endif
 
+
+#ifndef STM32_UART_DEBUG_ENABLED
+#define STM32_UART_DEBUG_ENABLED 1
+#endif
+
+#if STM32_UART_DEBUG_ENABLED
+#ifndef STM32_UART_DEBUG
+#define STM32_UART_DEBUG
+#endif
+#endif
+
+
 #ifndef M_PI
 #define M_PI 3.1415926F
 #endif
